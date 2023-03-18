6ff941c1754021b88d29a9ca91cbc95473db827d
Sarvesh <sarvesh20304@gmail.com>
2023-03-18 12:21:07 +0530

    Chase rearrangement

---
+++ Assets/Scripts/UI_Scripts/UI_Exit_script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Exit_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Quitgame()
+    {
+        Application.Quit();
+        Debug.Log("IT WORKS TEARS IN MY EYES CUM IN MY PANTS");
+    }
+}
